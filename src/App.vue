00346abdec9b9995bfe697221059c547dde56dba
Paco Navarro <packarbell@gmail.com>
2017-06-07 22:02:35 +0200

    save library

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img class="logo" src="./assets/empanadilla-recortada.png">
     <router-view></router-view>
   </div>
 </template>
@@ -19,5 +19,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo {
+    width: 300px;
 }
 </style>
---
+++ src/components/api/Api.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>API Library</h1>
+    {{library}}
+    <ul>  
+      <li><a href="#api">API Docs</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import library from './api-library.service';
+
+export default {
+  data() {
+    return {
+      library,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
